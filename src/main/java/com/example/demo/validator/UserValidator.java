acb3cf8fdd1324b954605b6bb17078a9b857bb38
ionutmedrea <ionutmedrea@yahoo.com>
2020-11-18 19:14:17 +0200

    analiza dinamica

--- src/main/java/com/example/demo/validator/UserValidator.java
+++ src/main/java/com/example/demo/validator/UserValidator.java
@@ -1,41 +1,32 @@
 package com.example.demo.validator;
 
+import com.example.demo.exceptions.UsernameAlreadyTakenException;
+import com.example.demo.exceptions.WrongUsernameOrPasswordException;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
-import org.springframework.validation.Validator;
+
 @Component
-public class UserValidator implements Validator {
+public class UserValidator {
 
     @Autowired
     private UserService userService;
 
-    @Override
-    public boolean supports(Class<?> aClass) {
-
-        return User.class.equals(aClass);
-    }
-
-    @Override
-    public void validate(Object o, Errors errors) {
+    public boolean validate(Object o) throws RuntimeException{
         User user = (User) o;
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"username","NotEmpty");
-
         if(user.getUsername().length()<6 || user.getUsername().length() > 32){
-            errors.rejectValue("username", "Size.userForm.username");
+            throw new WrongUsernameOrPasswordException("Wrong username");
         }
         if(userService.findByUsername(user.getUsername())!=null){
-            errors.rejectValue("username", "Duplicate.userForm.username");
+            throw new UsernameAlreadyTakenException("Username already taken");
         }
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"password","NotEmpty");
         if(user.getPassword().length() < 8 || user.getPassword().length()>32){
-            errors.rejectValue("password","Size.userForm.password");
+            throw new WrongUsernameOrPasswordException("Wrong password");
         }
-        if (!user.getPasswordConfirm().equals(user.getPassword())) {
-            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
+        if (!user.getConfirmPassword().equals(user.getPassword())) {
+            throw new WrongUsernameOrPasswordException("ConfirmPassword and password don't match");
         }
+        return true;
     }
 }
